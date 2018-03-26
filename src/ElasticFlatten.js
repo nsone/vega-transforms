b5fef03561287042c1491c829de7e7e82f0380dc
bcavns01 <bcavanagh@nsone.net>
2018-03-26 15:09:19 -0400

    Added some comments to make reading it less painful.

--- src/ElasticFlatten.js
+++ src/ElasticFlatten.js
@@ -50,17 +50,27 @@ function elasticFlatten(obj, leafNodeProperty, keyName="none") {
 	} else if(obj instanceof Object) {
 		var hashKey;
 		
+		/*
+		 * We're going to convert aggs into properties where the property name is the agg name and the value is the value of the "key" property of the object.
+		 * This is nice because then we don't have to care about the "key" property clobbring itself while recursion rolls back AND we implicitly control the property names
+		 * just by changing the elastic query.
+		 * 
+		 * For example, you have an agg named "the_date" and it contains buckets that have "key" properties unix timestamp as values.
+		 * Those buckets will then be given a property called "the_date" that will contain the unix timestamp found in their "key" properties.
+		*/
+		
 		if("key" in obj) {
 			obj[keyName] = obj.key;
 		}
 		
    		for (hashKey in obj) {
+   			// Short-circuiting another depth of the recursion if we know that the next thing is going to be the node we want.
 			if(hashKey == leafNodeProperty) {
 				incomingArrayOfHashes.push(obj[leafNodeProperty]);
 			} else if(obj[hashKey] instanceof Array) {
-				incomingArrayOfHashes = elasticFlatten(obj[hashKey], leafNodeProperty, keyName);
+				incomingArrayOfHashes = elasticFlatten(obj[hashKey], leafNodeProperty, keyName); // If it's an array, then they'll inherit the key name of the parent hash.
 			} else if(obj[hashKey] instanceof Object) {
-				incomingArrayOfHashes = elasticFlatten(obj[hashKey], leafNodeProperty, hashKey);
+				incomingArrayOfHashes = elasticFlatten(obj[hashKey], leafNodeProperty, hashKey); // If it's a hash, then pass along its "name" to the next level of recursion. 
 			} else {   		
 				myArrayHead[hashKey] = obj[hashKey];
 			}
@@ -70,14 +80,14 @@ function elasticFlatten(obj, leafNodeProperty, keyName="none") {
 	i = 0;
 	var incomingLen = incomingArrayOfHashes.length;
 	  
-	
+	// As the recursion rolls back, we need to add in the properties of the current depth to the incoming hashes.
 	if(incomingLen > 0) {
 		var myHeadKey;
 		var currentHash;
 		for(i = 0; i < incomingLen; i++) {
 			currentHash = incomingArrayOfHashes[i];
 			for (myHeadKey in myArrayHead) {
-			  currentHash[myHeadKey] = myArrayHead[myHeadKey];
+			  currentHash[myHeadKey] = myArrayHead[myHeadKey];  // Yes, duplicate key names means clobbering, but we shouldn't have to care.  If we start caring, we can start appending prefixes, but that would be terrible...
 			}
 		}
 	}
